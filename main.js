796a39f68afe0d8d986f0d86215c21ce57d69a30
Nopphadon Rakkanying <nopphadon@bakertilly.co.th>
2025-01-29 14:37:21 +0700

    Add fail page in main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ app.get("/", (req, res) => {
     res.send(`${"<h1><center style='font-size:50px;'>"}Hello World!!${"</center></h1>"}`);
 })
 
-app.get("/about", (req, res)=>{
+app.get("/fail", (req, res)=>{
     res.statusCode=500;
     res.send(`${"<h1><center style='font-size:50px; color: red;'>"}Fail Page!!${"</center></h1>"}`);
 })
