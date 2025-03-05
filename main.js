a5af2f7ad359055a8837ccd055df58a99de8c15e
sarawut <sarawut24554@gmail.com>
2025-03-05 13:56:15 +0700

    module.exports = server

--- main.js
+++ main.js
@@ -29,6 +29,8 @@ app.get("/metrics", async (req, res) => {
 });
 
 const port = process.env.PORT || 8888;
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+module.exports = server;
