9b656eac49481f11807d132aeca0e49a6e25be15
sarawut <sarawut24554@gmail.com>
2025-05-16 16:14:15 +0700

    delete matrice

--- main.js
+++ main.js
@@ -1,29 +1,15 @@
 const express = require("express");
 const app = express();
-const client = require("prom-client");
-
-const register = new client.Registry();
-client.collectDefaultMetrics({ register });
-
-const httpRequestCounter = new client.Counter({
-    name: "http_request_total",
-    help: "Total request count",
-    labelNames: ["method", "path", "status"]
-});
-
-register.registerMetric(httpRequestCounter);
 
 app.get("/", (req, res) => {
-    httpRequestCounter.inc({ method: 'GET', path: '/', status: '200' });
     res.send("<h1><center style='font-size:50px;'>Hello World!!</center></h1>");
 });
 
 app.get("/fail", (req, res) => {
-    httpRequestCounter.inc({ method: 'GET', path: 'fail', status: '500' });
     res.status(500).send("<h1><center style='font-size:50px; color: red;'>Fail Page!!</center></h1>");
 });
 
-module.exports = app; 
+module.exports = app;
 
 if (require.main === module) {
     const port = process.env.PORT || 8888;
